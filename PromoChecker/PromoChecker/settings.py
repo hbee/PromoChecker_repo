6c2737c7628f9067004b8884ff299397a9c2d746
Hamza Bendi <hamzabendi@gmail.com>
2023-03-15 17:43:06 +0100

    config sec settings

--- PromoChecker/PromoChecker/settings.py
+++ PromoChecker/PromoChecker/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j%zp^=qwq2@guq%%2f=kz0odx#ew7%qu81+pno(!ows)ptn0v('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
